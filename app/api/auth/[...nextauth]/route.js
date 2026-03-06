d5a19872cd1cd4cc32d8034e0a21c6a23a675f46
devpatrick007 <devpatrick007@gmail.com>
2026-03-06 21:44:10 +0000

    added Api's

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,73 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+
+            async authorize(credentials) {
+                try {
+                    const res = await fetch(
+                        "https://pellakes-backend.prospafin.com/api/auth/sign-in/email",
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                email: credentials.email,
+                                password: credentials.password,
+                            }),
+                        }
+                    )
+
+                    const data = await res.json()
+
+                    if (!res.ok || !data) {
+                        return null
+                    }
+
+                    return {
+                        ...data.user,
+                        token: data.token,
+                    }
+                } catch (error) {
+                    return null
+                }
+            }
+        }),
+    ],
+
+    session: {
+        strategy: "jwt",
+    },
+
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user
+                token.accessToken = user.token
+            }
+            return token
+        },
+
+        async session({ session, token }) {
+            session.user = token.user
+            session.accessToken = token.accessToken
+            return session
+        },
+    },
+
+    pages: {
+        signIn: "/dashboard",
+    },
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
