6e6e3d8644e584795ab33cebd7ad434f5fe2c2a7
devpatrick007 <devpatrick007@gmail.com>
2026-03-08 16:47:59 +0000

    reverted next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false, // disable Turbopack
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
+
+// // next.config.mjs
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     turbo: false, // disable Turbopack
+//   },
+// };
+
+// export default nextConfig;
